1b94d1f54bb7db74a28290c6b234c2009356d27d
Tom <tom@vibrato.com.au>
2017-12-28 10:34:15 +1100

    added cli params

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/TWinsnes/awscreds/console"
 )
 
 func main() {
 
-	err := console.OpenConsole("temptoken", "vibrato-tom")
+	consoleCommand := flag.NewFlagSet("console", flag.ExitOnError)
+
+	consoleProfileFlag := consoleCommand.String("profile", "Default", "AWS profile to log into console with")
+	consoleNameFlag := consoleCommand.String("tokenname", "temptoken", "Name of federated token")
+
+	if len(os.Args) < 2 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "console":
+		consoleCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	err := console.OpenConsole(*consoleNameFlag, *consoleProfileFlag)
 
 	if err != nil {
 		panic(err)
